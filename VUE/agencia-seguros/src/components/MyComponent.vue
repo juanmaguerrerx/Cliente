a95e2e1b54e4d07cb210c2c2340349f3b58aec93
juanmaguerrerx <jguevid1609@g.educaand.es>
2024-01-26 12:55:52 +0100

    helloWorld_vue_new

--- VUE/agencia-seguros/src/components/MyComponent.vue
+++ VUE/agencia-seguros/src/components/MyComponent.vue
@@ -1,1 +1,23 @@
-<template></template>
+<template>
+    <div class="my-component">
+        <h1 v-if="isVisible">{{ mensaje }}</h1>
+        <button v-on:click="cambiarVis">Cambiar visibilidad</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyComponet',
+    data() {
+        return {
+            mensaje: 'Hola lambebicho',
+            isVisible: true
+        }
+    },
+    methods: {
+        cambiarVis() {
+            this.isVisible = !this.isVisible;
+        }
+    }
+}
+</script>
